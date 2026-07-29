706d11110c42350bb1d649fb5aba5d916658e82c
ame-888 <greatalexanderbr@gmail.com>
2026-07-29 17:48:18 -0300

    Correct release data and aggregate score display

--- test/scoring-api.ts
+++ test/scoring-api.ts
@@ -1,5 +1,6 @@
 export * from '../src/data/benchmarkSpec';
 export * from '../src/data/scoring';
 export * from '../src/data/leaderboard';
+export * from '../src/data/formatting';
 export * from '../src/data/modelPricing';
 export { benchmarks } from '../src/data/benchmarks';
